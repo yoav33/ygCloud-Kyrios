107c93c15877f772efbafac281b5f1c6d6ec7f94
yoav33 <yoav.grinberg1@gmail.com>
2023-03-09 21:02:34 +0100

    socket initializing loop fix

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 # Kyrios, or kurios, is a Greek word which is usually translated as "lord" or "master".
 # https://github.com/yoav33
 
+print("--------------")
+print("ygCloud-Kyrios")
+print("--------------")
+
 import configparser
 import socket
 import os
@@ -23,29 +27,17 @@ HOST = 'localhost'
 PORT = (config.get('server', 'port'))
 PASSKEY = (config.get('server', 'passkey'))
 
-ACK_TEXT = 'text_received'
-
-
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((HOST, int(PORT)))
     sock.listen()
+    print()
     print('waiting for client connection...')
     conn, addr = sock.accept()      # Note: execution waits here until the client calls sock.connect()
     print('client connected, waiting for passkey')
     passkeymessage = 'sendpasskey'
     print(f"sending: {passkeymessage}")
     verifyPasskey(passkeymessage, conn, PASSKEY)
-
-    myCounter = 0
-    while True:
-        message = 'message ' + str(myCounter)
-        print('sending: ' + message)
-        sendTextViaSocket(message, conn)
-        myCounter += 1
-        time.sleep(1)
-    # end while
-# end function
 
 def verifyPasskey(send, sock, PASSKEY):
     encReq = bytes(send, 'utf-8') #encReq = encoded request
@@ -70,25 +62,6 @@ def verifyPasskey(send, sock, PASSKEY):
         encAcc = bytes("passkeyrejected", 'utf-8')
         sock.sendall(encAcc)
         print("passkey declined! sending rejection message and restarting")
-
-
-def sendTextViaSocket(message, sock):
-    # encode the text message
-    encodedMessage = bytes(message, 'utf-8')
-
-    # send the data via the socket to the server
-    sock.sendall(encodedMessage)
-
-    # receive acknowledgment from the server
-    encodedAckText = sock.recv(1024)
-    ackText = encodedAckText.decode('utf-8')
-
-    # log if acknowledgment was successful
-    if ackText == ACK_TEXT:
-        print('server acknowledged reception of text')
-    else:
-        print('error: server has sent back ' + ackText)
-    # end if
 
 while True:
     main()
